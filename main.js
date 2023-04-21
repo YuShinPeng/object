8c90d15f705979264620db2dce4da1f4dff59a1c
YuShinPeng <ccsauce173@gmail.com>
2023-04-21 16:39:37 +0800

    commit

---
+++ main.js
@@ -0,0 +1,460 @@
+// 物件
+// MAP Key : Value
+// 弱型別
+// const divorce = {
+//     "success": true,
+//     "result": {
+//     "resource_id": "301000000A-002072-001",
+//     "limit": 2000,
+//     "total": 45,
+//     "fields": [
+//     {
+//     "type": "text",
+//     "id": "statistic_yyy"
+//     },
+//     {
+//     "type": "text",
+//     "id": "according"
+//     },
+//     {
+//     "type": "text",
+//     "id": "district_code"
+//     },
+//     {
+//     "type": "text",
+//     "id": "site_id"
+//     },
+//     {
+//     "type": "text",
+//     "id": "sex"
+//     },
+//     {
+//     "type": "text",
+//     "id": "average_age"
+//     }
+//     ],
+//     "records": [
+//     {
+//     "statistic_yyy": "統計年",
+//     "according": "按照別",
+//     "district_code": "區域別代碼",
+//     "site_id": "區域別",
+//     "sex": "性別",
+//     "average_age": "平均年齡"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "65000000",
+//     "site_id": "新北市",
+//     "sex": "男",
+//     "average_age": "33.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "65000000",
+//     "site_id": "新北市",
+//     "sex": "女",
+//     "average_age": "31.1"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "63000000",
+//     "site_id": "臺北市",
+//     "sex": "男",
+//     "average_age": "33.9"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "63000000",
+//     "site_id": "臺北市",
+//     "sex": "女",
+//     "average_age": "32.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "68000000",
+//     "site_id": "桃園市",
+//     "sex": "男",
+//     "average_age": "31.9"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "68000000",
+//     "site_id": "桃園市",
+//     "sex": "女",
+//     "average_age": "30.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "66000000",
+//     "site_id": "臺中市",
+//     "sex": "男",
+//     "average_age": "32.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "66000000",
+//     "site_id": "臺中市",
+//     "sex": "女",
+//     "average_age": "30.2"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "67000000",
+//     "site_id": "臺南市",
+//     "sex": "男",
+//     "average_age": "32.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "67000000",
+//     "site_id": "臺南市",
+//     "sex": "女",
+//     "average_age": "30.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "64000000",
+//     "site_id": "高雄市",
+//     "sex": "男",
+//     "average_age": "32.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "64000000",
+//     "site_id": "高雄市",
+//     "sex": "女",
+//     "average_age": "30.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10002000",
+//     "site_id": "宜蘭縣",
+//     "sex": "男",
+//     "average_age": "31.4"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10002000",
+//     "site_id": "宜蘭縣",
+//     "sex": "女",
+//     "average_age": "29.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10004000",
+//     "site_id": "新竹縣",
+//     "sex": "男",
+//     "average_age": "31.8"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10004000",
+//     "site_id": "新竹縣",
+//     "sex": "女",
+//     "average_age": "30.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10005000",
+//     "site_id": "苗栗縣",
+//     "sex": "男",
+//     "average_age": "31.6"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10005000",
+//     "site_id": "苗栗縣",
+//     "sex": "女",
+//     "average_age": "29.8"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10007000",
+//     "site_id": "彰化縣",
+//     "sex": "男",
+//     "average_age": "31.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10007000",
+//     "site_id": "彰化縣",
+//     "sex": "女",
+//     "average_age": "29.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10008000",
+//     "site_id": "南投縣",
+//     "sex": "男",
+//     "average_age": "31.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10008000",
+//     "site_id": "南投縣",
+//     "sex": "女",
+//     "average_age": "29.2"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10009000",
+//     "site_id": "雲林縣",
+//     "sex": "男",
+//     "average_age": "31.1"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10009000",
+//     "site_id": "雲林縣",
+//     "sex": "女",
+//     "average_age": "29.1"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10010000",
+//     "site_id": "嘉義縣",
+//     "sex": "男",
+//     "average_age": "31.2"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10010000",
+//     "site_id": "嘉義縣",
+//     "sex": "女",
+//     "average_age": "29.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10013000",
+//     "site_id": "屏東縣",
+//     "sex": "男",
+//     "average_age": "31.7"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10013000",
+//     "site_id": "屏東縣",
+//     "sex": "女",
+//     "average_age": "29.4"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10014000",
+//     "site_id": "臺東縣",
+//     "sex": "男",
+//     "average_age": "31.2"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10014000",
+//     "site_id": "臺東縣",
+//     "sex": "女",
+//     "average_age": "29.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10015000",
+//     "site_id": "花蓮縣",
+//     "sex": "男",
+//     "average_age": "31.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10015000",
+//     "site_id": "花蓮縣",
+//     "sex": "女",
+//     "average_age": "29.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10016000",
+//     "site_id": "澎湖縣",
+//     "sex": "男",
+//     "average_age": "31.2"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10016000",
+//     "site_id": "澎湖縣",
+//     "sex": "女",
+//     "average_age": "29.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10017000",
+//     "site_id": "基隆市",
+//     "sex": "男",
+//     "average_age": "32.4"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10017000",
+//     "site_id": "基隆市",
+//     "sex": "女",
+//     "average_age": "30.6"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10018000",
+//     "site_id": "新竹市",
+//     "sex": "男",
+//     "average_age": "32.6"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10018000",
+//     "site_id": "新竹市",
+//     "sex": "女",
+//     "average_age": "30.7"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10020000",
+//     "site_id": "嘉義市",
+//     "sex": "男",
+//     "average_age": "32.4"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10020000",
+//     "site_id": "嘉義市",
+//     "sex": "女",
+//     "average_age": "30.1"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "09020000",
+//     "site_id": "金門縣",
+//     "sex": "男",
+//     "average_age": "31.7"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "09020000",
+//     "site_id": "金門縣",
+//     "sex": "女",
+//     "average_age": "30.1"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "09007000",
+//     "site_id": "連江縣",
+//     "sex": "男",
+//     "average_age": "32.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "09007000",
+//     "site_id": "連江縣",
+//     "sex": "女",
+//     "average_age": "30.0"
+//     }
+//     ]
+//     }
+//     }
+
+// console.log(divorce.result.records[1].average_age);
+
+// divorce.result.records.forEach(function(i){
+//     console.log(i.average_age);
+// })
+
+
+
+
+
+// const person = {
+//     name : "John",
+//     age : 23,
+//     tel:[   "02-222222222",
+//             "0987-654-321"
+//     ],
+//     email: [ "1@email.com",
+//              "2@email.com"
+//     ],
+//     address: "asdqwezxc",
+//     pet:[{
+//         name:"mary",
+//         age:6
+//     },{
+//         name:"katty",
+//         age:9
+//     }]
+    
+// }
+
+// console.log(person.tel[0]);
+
+
+// person.pet.forEach(function(i){
+//     console.log(i.age);
+// });
+
+
+// 變數範圍
+const name1 = "全域"
+
+const obj = {
+    name1:"obj的name",
+  
+}
+
+function fn(){
+    const name1 = "fn的name";
+    console.log(name1);
+}
+
+console.log(name1);
+console.log(obj.name1);
+fn();
+
+// function fn2(){
+//     console.log("執行fn2方法")
+// }
+
+// fn2();
